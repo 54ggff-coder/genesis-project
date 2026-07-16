c428f4077d35d5fea8c17a4b149b7fd3a40ab08e
Alifwaad <54ggff@gmail.com>
2026-07-17 02:46:43 +0300

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,8 @@
 import { supabase } from "./supabase";
 
-export async function signOut(){
+export async function signIn( email: string, password: string ) { return await supabase.auth.signInWithPassword({ email, password, }); }
 
-await supabase.auth.signOut();
+export async function signUp( email: string, password: string ) { return await supabase.auth.signUp({ email, password, }); }
 
-}
+export async function signOut() { return await supabase.auth.signOut(); }
+
