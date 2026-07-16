194e28126728325192f87f016097cd1b767f13ba
Alifwaad <54ggff@gmail.com>
2026-07-17 02:45:49 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =https://wshbywurteipkpsdrvlm.supabase.co
-process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const url =https://wshbywurteipkpsdrvlm.supabase.co process.env.NEXT_PUBLIC_SUPABASE_URL!; const key =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzaGJ5d3VydGVpcGtwc2RydmxtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NDEzMTA3NSwiZXhwIjoyMDk5NzA3MDc1fQ.Js8xXxCDGeUgLlC1iEJA76xUetWGi1PSh9euN8YH0rc process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supabaseAnonKey =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzaGJ5d3VydGVpcGtwc2RydmxtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NDEzMTA3NSwiZXhwIjoyMDk5NzA3MDc1fQ.Js8xXxCDGeUgLlC1iEJA76xUetWGi1PSh9euN8YH0rc
-process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+export const supabase = createClient(url, key);
 
-export const supabase =wshbywurteipkpsdrvlm.supabase.co
-createClient(
-supabaseUrl,
-supabaseAnonKey
-);
