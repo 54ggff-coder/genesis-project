8866c61de7942c9102aa5c3bea2d033c647a8914
Alifwaad <54ggff@gmail.com>
2026-07-17 02:06:47 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,33 @@
-.
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center p-6 border-b bg-white">
+
+      <Link href="/" className="font-bold text-2xl">
+        Genesis
+      </Link>
+
+      <div className="flex gap-5">
+
+        <Link href="/">
+          Home
+        </Link>
+
+        <Link href="/assessment">
+          Assessment
+        </Link>
+
+        <Link href="/dashboard">
+          Dashboard
+        </Link>
+
+        <Link href="/login">
+          Login
+        </Link>
+
+      </div>
+
+    </nav>
+  );
+}
