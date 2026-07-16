a5a239e8a878a1932fb02a02f7640bd8ee86ca8a
Alifwaad <54ggff@gmail.com>
2026-07-17 02:05:33 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
-.
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Project Genesis",
+  description: "Discover your hidden potential.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
