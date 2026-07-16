1b39c393fd37cbcab49a7210de285722e936dea5
Alifwaad <54ggff@gmail.com>
2026-07-17 02:58:43 +0300

    Update Input.tsx

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,1 +1,43 @@
-.
+"use client";
+
+export default function Input({
+
+placeholder,
+
+type="text",
+
+value,
+
+onChange,
+
+}:{
+
+placeholder:string;
+
+type?:string;
+
+value:string;
+
+onChange:(e:any)=>void;
+
+}){
+
+return(
+
+<input
+
+className="w-full border rounded-xl p-3"
+
+placeholder={placeholder}
+
+type={type}
+
+value={value}
+
+onChange={onChange}
+
+/>
+
+);
+
+}
