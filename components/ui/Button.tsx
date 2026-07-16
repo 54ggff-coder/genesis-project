876860a9736300464c355cf6237a6635881f87db
Alifwaad <54ggff@gmail.com>
2026-07-17 02:57:54 +0300

    Update Button.tsx

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,1 +1,35 @@
-.
+"use client";
+
+export default function Button({
+
+children,
+
+onClick,
+
+type="button",
+
+}:{
+
+children:React.ReactNode;
+
+onClick?:()=>void;
+
+type?:"button"|"submit";
+
+}){
+
+return(
+
+<button
+
+type={type}
+
+onClick={onClick}
+
+className="bg-black text-white rounded-xl px-5 py-3 hover:opacity-90 transition"
+
+{children}
+
+);
+
+}
