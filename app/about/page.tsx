f61bea3a77eb630ff5bc301e7cfa7c5cd0c38efb
Alifwaad <54ggff@gmail.com>
2026-07-17 06:16:56 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,16 @@
 export default function AboutPage() {
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">About</h1>
-      <p>Coming soon.</p>
+    <main className="max-w-5xl mx-auto py-20">
+
+      <h1 className="text-5xl font-bold">
+        About Project Genesis
+      </h1>
+
+      <p className="mt-6 text-lg">
+        Project Genesis helps people discover
+        their hidden abilities and talents.
+      </p>
+
     </main>
   );
 }
