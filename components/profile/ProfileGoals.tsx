f3383a9903961d7e45545317aee7d39946a95b41
Alifwaad <54ggff@gmail.com>
2026-07-17 06:47:02 +0300

    Create ProfileGoals.tsx

---
+++ components/profile/ProfileGoals.tsx
@@ -0,0 +1,23 @@
+export default function ProfileGoals(){
+
+return(
+
+<div className="border rounded-2xl p-8">
+
+<h2 className="text-2xl font-bold">
+
+Goals
+
+</h2>
+
+<p className="mt-4">
+
+Complete assessments to unlock your goals.
+
+</p>
+
+</div>
+
+);
+
+}
