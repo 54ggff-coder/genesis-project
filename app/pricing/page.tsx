dc04d2a52a279455fd9461278265b0cd4b1dd2c3
Alifwaad <54ggff@gmail.com>
2026-07-17 06:19:26 +0300

    Create pricing/page.tsx

---
+++ app/pricing/page.tsx
@@ -0,0 +1,15 @@
+export default function PricingPage() {
+  return (
+    <main className="max-w-5xl mx-auto py-20">
+
+      <h1 className="text-5xl font-bold">
+        Premium Plans
+      </h1>
+
+      <p className="mt-6">
+        Upgrade to unlock advanced reports.
+      </p>
+
+    </main>
+  );
+}
