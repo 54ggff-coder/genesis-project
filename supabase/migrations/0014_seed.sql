3e9c429595d376df87519ff74f05ec8db90d8091
Alifwaad <54ggff@gmail.com>
2026-07-17 07:33:47 +0300

    Create supabase/migrations/0014_seed.sql

---
+++ supabase/migrations/0014_seed.sql
@@ -0,0 +1,23 @@
+insert into skills(name,description)
+
+values
+
+('Leadership','Ability to lead others'),
+
+('Creativity','Creative thinking'),
+
+('Communication','Communication skills'),
+
+('Problem Solving','Logical thinking'),
+
+('Teamwork','Working with teams'),
+
+('Entrepreneurship','Business mindset'),
+
+('Discipline','Self discipline'),
+
+('Learning','Fast learner'),
+
+('Innovation','Innovation'),
+
+('Decision Making','Decision making');
