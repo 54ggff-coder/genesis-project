80e38a21f1e164db94a65741b85c0d8d8cd52d6e
Alifwaad <54ggff@gmail.com>
2026-07-17 06:17:50 +0300

    Update page.tsx

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,1 +1,15 @@
-.
+export default function TermsPage() {
+  return (
+    <main className="max-w-5xl mx-auto py-20">
+
+      <h1 className="text-5xl font-bold">
+        Terms of Service
+      </h1>
+
+      <p className="mt-6">
+        Terms will appear here.
+      </p>
+
+    </main>
+  );
+}
