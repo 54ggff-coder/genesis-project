f88688b0e96b8e0049a8a8c9b65ded163b6a08b1
Alifwaad <54ggff@gmail.com>
2026-07-17 06:46:40 +0300

    Create ProfileSkills.tsx

---
+++ components/profile/ProfileSkills.tsx
@@ -0,0 +1,29 @@
+export default function ProfileSkills(){
+
+return(
+
+<div className="border rounded-2xl p-8">
+
+<h2 className="text-2xl font-bold mb-6">
+
+Top Skills
+
+</h2>
+
+<ul className="space-y-3">
+
+<li>Leadership</li>
+
+<li>Problem Solving</li>
+
+<li>Communication</li>
+
+<li>Creativity</li>
+
+</ul>
+
+</div>
+
+);
+
+}
