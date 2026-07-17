c5859ca5085396d94168e8f353465d624e2c30ce
Alifwaad <54ggff@gmail.com>
2026-07-17 03:04:04 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,16 @@
 export default function Footer() {
   return (
-    <footer className="border-t mt-20 py-10 text-center text-gray-500">
+    <footer className="border-t mt-20">
 
-      © 2026 Project Genesis
+      <div className="max-w-7xl mx-auto py-10 px-6">
+
+        <p className="text-center text-gray-500">
+
+          © 2026 Project Genesis
+
+        </p>
+
+      </div>
 
     </footer>
   );
