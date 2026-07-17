e70100393ae55dd0dd144ebbcecc917d5bfb8726
Alifwaad <54ggff@gmail.com>
2026-07-17 06:25:59 +0300

    Create layout/PageHeader.tsx

---
+++ components/layout/PageHeader.tsx
@@ -0,0 +1,25 @@
+type Props = {
+  title: string;
+  description?: string;
+};
+
+export default function PageHeader({
+  title,
+  description,
+}: Props) {
+  return (
+    <div className="mb-10">
+
+      <h1 className="text-5xl font-bold">
+        {title}
+      </h1>
+
+      {description && (
+        <p className="mt-4 text-gray-500">
+          {description}
+        </p>
+      )}
+
+    </div>
+  );
+}
