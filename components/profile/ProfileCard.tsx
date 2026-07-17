e468c4e0c1ef0a9f425dd97a25907299422c5593
Alifwaad <54ggff@gmail.com>
2026-07-17 06:45:53 +0300

    Create ProfileCard.tsx

---
+++ components/profile/ProfileCard.tsx
@@ -0,0 +1,35 @@
+export default function ProfileCard(){
+
+return(
+
+<div className="border rounded-2xl p-8">
+
+<h2 className="text-2xl font-bold mb-6">
+
+Basic Information
+
+</h2>
+
+<p><strong>Name:</strong> User</p>
+
+<p className="mt-3">
+
+<strong>Email:</strong>
+
+user@email.com
+
+</p>
+
+<p className="mt-3">
+
+<strong>Country:</strong>
+
+Unknown
+
+</p>
+
+</div>
+
+);
+
+}
