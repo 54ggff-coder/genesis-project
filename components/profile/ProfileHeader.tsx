3a54d527e0f333b2ab88d11014fc15e3887e6257
Alifwaad <54ggff@gmail.com>
2026-07-17 06:44:59 +0300

    Create components/profile/ProfileHeader.tsx

---
+++ components/profile/ProfileHeader.tsx
@@ -0,0 +1,23 @@
+export default function ProfileHeader(){
+
+return(
+
+<div>
+
+<h1 className="text-5xl font-bold">
+
+My Profile
+
+</h1>
+
+<p className="mt-4 text-gray-600">
+
+Manage your personal information.
+
+</p>
+
+</div>
+
+);
+
+}
